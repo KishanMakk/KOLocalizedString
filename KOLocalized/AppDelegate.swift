df4fdfb0eaf54c0c9560c97993757701c12ba60d
Oleksandr <art@khymych.com>
2016-11-24 22:27:31 +0200

    Update AppDelegate.swift

--- KOLocalized/AppDelegate.swift
+++ KOLocalized/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        KOLocalizedSettings(nameFile: nil, appLocaleList: ["en"], defaultLocale: "en", localeDevice: false)
+        KOLocalizedSettings(nameFile: nil, appLocaleList: ["en","ru"], defaultLocale: "en", localeDevice: false)
         return true
     }
 
